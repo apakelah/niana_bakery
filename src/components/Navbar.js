9a4d1fd1b8a2d5121264cd3fa8d7c0718f89d49e
apakelah <hanyamahasiswa604@gmail.com>
2025-04-21 17:12:15 +0700

    update pertemuan 5

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "gatsby";
-import github from "../img/github-icon.svg";
+import github from "../img/whatshap.png";
 import logo from "../img/go.png";
 
 
@@ -59,20 +59,20 @@ const Navbar = () => {
               Contact
             </Link>
           </li>
-          <li className="navbar-item" style={{ padding: "0px" }}>
+          {/* <li className="navbar-item" style={{ padding: "0px" }}>
             <Link className="navbar-item" to="/contact/examples">
               Form Examples
             </Link>
-          </li>
+          </li> */}
           <li className="navbar-end has-text-centered">
             <a
               className="navbar-item"
-              href="https://github.com/decaporg/gatsby-plugin-decap-cms"
+              href="https://wa.me/6288212538241" 
               target="_blank"
               rel="noopener noreferrer"
             >
               <span className="icon">
-                <img src={github} alt="Github" />
+                <img src={github} alt="whatshap" />
               </span>
             </a>
           </li>
